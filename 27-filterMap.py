a800e42e5ae4e9736f2dfefb2ec50ab359005061
edgardo <ecometto@hotmail.com>
2024-04-19 11:40:44 -0300

    course updated untill class 53 - Filter

---
+++ 27-filterMap.py
@@ -0,0 +1,48 @@
+
+#forma tradicional de filtrar (sin filter function)
+edades = [11,22,51,43,10,65]
+
+def mayorEdad(edades):
+    mayores = []
+    for edad in edades:
+        if edad > 18:
+            mayores.append(edad)
+    return mayores
+
+mayores = mayorEdad(edades)
+print(mayores)
+
+# --------------------------------------------------------------------------------------------------
+# filtrado usando filter
+#primero se define la funcion que, según la condicion indicada, retornará True o False
+def mayor(edad):
+    return edad > 18
+
+#luego se aplica la funcion generada, a la lista de objetos iterables. Parametros: (función, objeto iterable)
+mayoresFilter = filter(mayor, edades)
+print(mayoresFilter) 
+#dado que retorna un objeto filter, se debe transformar a lista
+mayoresFilterLista = list(filter(mayor, edades))
+print(mayoresFilterLista)
+
+#-------------------------------------------------------------------------
+# #otro ejemplo
+print("----------------------------------------")
+
+class Persona():
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+        
+    def __str__(self):
+        return f"{self.nombre} tiene {self.edad} anios"
+        
+listaPersonas2 = [ Persona("juana", 30), Persona("pedro", 20), Persona("maria", 55), Persona("lucas", 32) ]
+
+def mayores30(persona):
+    return persona.edad > 30
+
+
+personasMas30 = list(filter(mayores30, listaPersonas2))
+for persona in personasMas30:
+    print(persona)
---
+++ curso/26-lambdas.py
@@ -0,0 +1,15 @@
+
+#forma tradicional:
+def sumar(a,b):
+    return a+b
+
+print(sumar(8,3))
+
+
+
+#funcion lambda: funciones de una sola linea, con codigo simplificado.
+suma = lambda a,b : a+b
+print(suma(4, 55))
+
+potenciar = lambda a, n : a**n
+print(potenciar(5,3))
